2a0b0b3b35d147b0a3fcf621f148231e5e0369a4
rajat-mehra05 <rajat.m.5278@gmail.com>
2022-06-03 13:40:48 +0530

    fix: server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const dotenv = require("dotenv");
 dotenv.config();
 const morgan = require("morgan");
 const connectDB = require("./config/db");
-const PORT = process.env.PORT || 8080;
 
 connectDB();
 
@@ -15,10 +14,6 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
-app.listen(PORT, () => {
-  console.log(`Server is running at ${PORT}`.yellow.bold);
-});
-
 //models
 require("./models/userModel");
 require("./models/postModel");
@@ -27,3 +22,9 @@ require("./models/postModel");
 app.use(require("./routes/authRoutes"));
 app.use(require("./routes/postRoutes"));
 app.use(require("./routes/userRoutes"));
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`.yellow.bold);
+});
