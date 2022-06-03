eb17d379dc51f1eb926a09de3fd46c3b5c12f3b9
Rajat Mehra <rajat.m.5278@gmail.com>
2022-06-03 20:29:44 +0530

    added frontend link

--- config/cors.js
+++ config/cors.js
@@ -13,7 +13,7 @@ OPTIONS request.
  * The `exposedHeaders` property is an array of headers
  */
 export const corsOptions = {
-  origin: ["http://localhost:3000", "https://socialhq-api.up.railway.app/"],
+  origin: ["http://localhost:3000", "https://social-hq.vercel.app/", "https://socialhq-api.up.railway.app/"],
   optionsSuccessStatus: 200,
   credentials: true,
   exposedHeaders: ["set-cookie"],
